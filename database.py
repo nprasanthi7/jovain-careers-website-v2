7c46b386a95a4cf4005d2b438fea103d8584dc30
Prasanthi N <prasanthi.vvit@gmail.com>
2022-08-31 17:59:47 +0000

    added form

--- database.py
+++ database.py
@@ -26,3 +26,8 @@ def load_job_from_db(id):
       return None
     else:
       return dict(rows[0])
+def add_application_db(job_id,data):
+  with engine.connect() as conn:
+    query=text('insert into applications (job_id, full_name, email,linkedin_url,education,work_experience,resume_url) values (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)')
+    conn.execute(query,job_id=job_id,                 full_name=data['full_name'], 
+         email=data['email'], linkedin_url=data['linkedin_url'], education=data['education'], work_experience=data['work_experience'], resume_url=data['resume_url'])
